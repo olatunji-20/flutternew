c33864d78169206d6de0b794b2352526a29b13a9
falana sheriff <falanasheriffdeen@gmail.com>
2023-02-27 09:48:13 +0100

    testing out flutterwave

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <div>
+    <flutterwave-pay-button   v-bind="paymentData" > Click To Pay </flutterwave-pay-button>
+ </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  data(){
+    return {
+      paymentData: {
+        tx_ref: this.generateReference(),
+        amount: 10,
+        currency: 'NGN',
+        payment_options: 'card,ussd',
+        redirect_url: '',
+        meta: {
+          'counsumer_id': '7898',
+          'consumer_mac': 'kjs9s8ss7dd'
+        },
+        customer: {
+          name: 'Demo Customer  Name',
+          email: 'customer@mail.com',
+          phone_number: '0818450***44'
+        } ,
+        customizations: {
+          title: 'Customization Title',
+          description: 'Customization Description',
+          logo: 'https://flutterwave.com/images/logo-colored.svg'
+        },
+        callback: this.makePaymentCallback,
+        onclose: this.closedPaymentModal
+      }
+    }
+  } ,
+  methods: {
+    makePaymentCallback(response) {
+      console.log("Pay", response)
+    },
+    closedPaymentModal() {
+      console.log('payment is closed');
+    },
+    generateReference(){
+      let date = new Date()
+      return date.getTime().toString();
+    }
+  }
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import Flutterwave from  'flutterwave-vue-v3'
+
+
+Vue.use(Flutterwave, { publicKey: 'FLWPUBK-5f8b0678c717359fb8a051039e1f30bc-X' } )
+
 Vue.config.productionTip = false
 
 new Vue({
